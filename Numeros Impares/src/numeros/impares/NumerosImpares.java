7a5bbe1812601030dab34729e969f944a95b8bbb
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-08-26 21:46:05 +0200

    Numeros Impares

---
+++ Numeros Impares/src/numeros/impares/NumerosImpares.java
@@ -0,0 +1,22 @@
+/**
+ * Descricao: Um programa que imprime numeros impares no intervalo de 0-100
+ * Data: 26/08/2021
+ */
+package numeros.impares;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+
+public class NumerosImpares {
+
+    public static void main(String[] args) {
+        for (int i = 0; i<100; i++){
+        if (i%2!=0){
+        System.out.println(i);
+     
+    }
+        }
+    }
+}
