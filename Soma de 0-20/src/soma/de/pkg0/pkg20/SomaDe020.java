7a041ab6482b60dcabe3993ef07f344fc2eb6ccc
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-08-28 21:19:12 +0200

    Soma de 0-20

---
+++ Soma de 0-20/src/soma/de/pkg0/pkg20/SomaDe020.java
@@ -0,0 +1,18 @@
+
+package soma.de.pkg0.pkg20;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class SomaDe020 {
+
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 0; i <= 20; i++) {
+            soma +=i;
+        }
+        System.out.println("A soma de todos numeros compreendidos de 0 a 20 é "+soma);
+    }
+    
+}
