918556f7bb00a3648d09105a55828f3e4b3ef878
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-14 23:51:48 +0200

    Contador de 1 ate 100

---
+++ Contador de 1 ate 100/src/contador/Contador_1_ate_100.java
@@ -0,0 +1,22 @@
+/**
+ * Contador de numeros naturais ate 100
+ * Data:02/06/2021
+ */
+package contador;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Contador_1_ate_100 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
