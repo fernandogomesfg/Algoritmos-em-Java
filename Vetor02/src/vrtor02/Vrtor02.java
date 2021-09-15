37a070ade3e046a8b86575819ab88b758a8381cf
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-16 01:29:48 +0200

    Updating property

--- Vetor02/src/vrtor02/Vrtor02.java
+++ Vetor02/src/vrtor02/Vrtor02.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Algoritmo sobre vetor construido com base no curo de Java para iniciantes do canal Curso em Video
+ * Data:16/0/2021
  */
 package vrtor02;
 
