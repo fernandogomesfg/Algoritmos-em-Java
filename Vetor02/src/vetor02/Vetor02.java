16f4b65cdc24e9af639a6afa8f8993893781f65e
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-16 01:42:15 +0200

    Update Vetor02.java

--- Vetor02/src/vetor02/Vetor02.java
+++ Vetor02/src/vetor02/Vetor02.java
@@ -14,7 +14,12 @@ public class Vetor02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String mes[] = {"Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        int dias[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int i = 0; i < mes.length; i++) {
+            System.out.println("O mes de " + mes[i] + " tem " + dias[i]+ " dias");
+        }
     }
-    
+
 }
