7c6ad7d464c6c39d7c759bf89d1f528de3378787
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-16 01:12:58 +0200

    vetor01

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,20 @@
+/**
+ * Algoritmo sobre vetor construido com base no curo de Java para iniciantes do canal Curso em Video
+ * Data:16/0/2021
+ */
+package vetor01;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n[] = {1,8,9,5};
+    }
+    
+}
