7a48ded7da9770dc3705db321fe03b1732066b73
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-16 01:31:37 +0200

    Create Vetor02.java

---
+++ Vetor02/src/vetor02/Vetor02.java
@@ -0,0 +1,20 @@
+/**
+ * Algoritmo sobre vetor construido com base no curo de Java para iniciantes do canal Curso em Video
+ * Data:16/0/2021
+ */
+package vetor02;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Vetor02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
