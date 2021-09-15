7a1c88895042ac19779bb9a0577cbf3dc4edf1bc
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-16 01:27:32 +0200

    Update Vetor01.java

--- Vetor01/src/vetor01/Vetor01.java
+++ Vetor01/src/vetor01/Vetor01.java
@@ -14,7 +14,15 @@ public class Vetor01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n[] = {1,8,9,5};
+        int n[] = {1, 8, 9, 5, 1, 5, 7, 75, 75, 7};
+
+        //Mostrar o tamanho do vetor
+        System.out.println("O tamanho do vetor e " + n.length);
+
+        //Mostrar os elementos do vetor
+        for (int i = 0; i <= n.length; i++) {
+            System.out.println("Na posicao " + i + " temos o valor " + n[i]);
+        }
     }
-    
+
 }
