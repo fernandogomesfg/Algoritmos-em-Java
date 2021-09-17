00367bee2532c724bd2ad807f6570ea7b4337fee
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-17 22:21:32 +0200

    Funcao

---
+++ Funcao/src/funcao/Funcao.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package funcao;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Funcao {
+
+    /**
+     * @param args the command line arguments
+     */
+    //Procedimento de soma
+    static void Soma(int a, int b) {
+        int s = a + b;
+        System.out.println(s);
+
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Soma(5,4);
+    }
+
+}
