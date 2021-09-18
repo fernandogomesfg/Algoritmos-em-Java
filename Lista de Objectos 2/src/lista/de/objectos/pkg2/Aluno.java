7434a0d8c859aef4702e5e1ec5c1806c42bf5201
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-19 00:29:28 +0200

    update

--- Lista de Objectos 2/src/lista/de/objectos/pkg2/Aluno.java
+++ Lista de Objectos 2/src/lista/de/objectos/pkg2/Aluno.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Lista de Objectos com classe Aluno
+ * Data: 19/09/2021
  */
 package lista.de.objectos.pkg2;
 
@@ -10,5 +9,48 @@ package lista.de.objectos.pkg2;
  * @author Fernando Gomes
  */
 public class Aluno {
-    
+
+    private String Nome;
+    private String Matricula;
+    private String Telefone;
+
+    //Construtor
+    public Aluno() {
+    }
+
+    public Aluno(String Nome, String Matricula, String Telefone) {
+        this.Nome = Nome;
+        this.Matricula = Matricula;
+        this.Telefone = Telefone;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    //Metodo para imprimir
+    public String getDados() {
+        return "Nome: " + this.Nome + "\nMatricula: " + this.Matricula + "\nTelefone: " + this.Telefone + "\n";
+    }
+
 }
--- Lista de Objectos 2/src/lista/de/objectos/pkg2/ListaDeObjectos2.java
+++ Lista de Objectos 2/src/lista/de/objectos/pkg2/ListaDeObjectos2.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Lista de Objectos com classe Aluno
+ * Data: 19/09/2021
  */
 package lista.de.objectos.pkg2;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -15,7 +16,27 @@ public class ListaDeObjectos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Aluno> ListaAlunos = new ArrayList();
+        Aluno a1 = new Aluno();
+        a1.setNome("Fernando Gomes");
+        a1.setMatricula("1111");
+        a1.setTelefone("841111");
+        ListaAlunos.add(a1);
+        
+        Aluno a2 = new Aluno();
+        a2.setNome("Gomes");
+        a2.setMatricula("2222");
+        a2.setTelefone("82222");
+        ListaAlunos.add(a2);
+        
+        Aluno a3 = new Aluno("Fernando", "33333","8333");
+        ListaAlunos.add(a3);
+        
+        ListaAlunos.add(new Aluno("Paulo", "4444", "840404"));
+        
+        for (int i = 0; i < ListaAlunos.size(); i++) {
+            System.out.println(ListaAlunos.get(i).getDados());
+        }
     }
     
 }
