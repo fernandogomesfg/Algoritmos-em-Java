974cdf1ce6fce9a8b74ac8725cd1597ec138a2be
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-18 22:31:06 +0200

    Create Aluno.java

---
+++ Orientacao a Objectos 2/src/orientacao/a/objectos/pkg2/Aluno.java
@@ -0,0 +1,14 @@
+/**
+ * Orientecao a objectos com classe Aluno
+ * Data: 18/09/2021
+ */
+package orientacao.a.objectos.pkg2;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Aluno {
+    String nome
+    
+}
