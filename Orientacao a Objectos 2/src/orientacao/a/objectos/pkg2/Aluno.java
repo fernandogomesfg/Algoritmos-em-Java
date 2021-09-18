8cc1e70b3aab636198686e600f4fb1c42cd3a17c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-18 23:03:22 +0200

    update

--- Orientacao a Objectos 2/src/orientacao/a/objectos/pkg2/Aluno.java
+++ Orientacao a Objectos 2/src/orientacao/a/objectos/pkg2/Aluno.java
@@ -9,6 +9,49 @@ package orientacao.a.objectos.pkg2;
  * @author Fernando Gomes
  */
 public class Aluno {
-    String nome
+
+    private String nome;
+    private String numero;
+    private String matricula;
+
+    //Construtor aluno
+    //O aluno deve ser criado com um nome --Aluno a1 = new Aluno("[nome do aluno]")
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    //Funcao/metodo para imprimir dados com objecto ALUNO
+    public void ImprimirDados() {
+        System.out.println("Nome: " + this.getNome() + "\nNumero: " + this.getNumero() + "\nMatricula: " + this.getMatricula());
+
+    }
     
+    public static double Media(double nota1, double nota2){
+        return (nota1 + nota2)/2;
+    }
+
 }
--- Orientacao a Objectos 2/src/orientacao/a/objectos/pkg2/OrientacaoAObjectos2.java
+++ Orientacao a Objectos 2/src/orientacao/a/objectos/pkg2/OrientacaoAObjectos2.java
@@ -15,7 +15,28 @@ public class OrientacaoAObjectos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Aluno a1 = new Aluno("Fernando Gomes");
+        a1.setNumero("1111111111111");
+        a1.setMatricula("00000000000001");
+
+        Aluno a2 = new Aluno("Pedro Jose");
+        a2.setNumero("2222222222222");
+        a2.setMatricula("0000000000002");
+
+        /*
+        System.out.println("Aluno 1\nNome: " + a1.getNome() + "\nNumero: " + a1.getNumero() + "\nMatricula: " + a1.getMatricula());
+        System.out.println("");
+        System.out.println("Aluno 2\nNome: " + a2.getNome() + "\nNumero: " + a2.getNumero() + "\nMatricula: " + a2.getMatricula());
+         */
+        a1.ImprimirDados();
+        System.out.println("");
+        a2.ImprimirDados();
+        double media = Aluno.Media(10,14);
+        System.out.println("Media: " +media);
+        
+       
     }
+
     
+
 }
