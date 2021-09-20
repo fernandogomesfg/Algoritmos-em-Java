9840bdb2336699ee48a91385f76bce398380d22c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-20 09:20:44 +0200

    ola mundo

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,21 @@
+
+package olamundo;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Ola Mundo");
+        
+        
+    }
+    
+}
