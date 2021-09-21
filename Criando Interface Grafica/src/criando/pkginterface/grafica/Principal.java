af3a8beda1659d8080d1ce42411aee35e0b7fcdb
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-21 23:24:42 +0200

    update

--- Criando Interface Grafica/src/criando/pkginterface/grafica/Principal.java
+++ Criando Interface Grafica/src/criando/pkginterface/grafica/Principal.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Data: 21/09/2021
  */
 package criando.pkginterface.grafica;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,6 +16,9 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
+
+        //Fazendo a tela abrir no centro
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -27,21 +30,102 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        grupo_sexo = new javax.swing.ButtonGroup();
+        txt_nome = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        btn_enviar = new javax.swing.JButton();
+        chb_idade = new javax.swing.JCheckBox();
+        rb_masc = new javax.swing.JRadioButton();
+        rb_fem = new javax.swing.JRadioButton();
+        jLabel2 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Nome");
+
+        btn_enviar.setText("Enviar");
+        btn_enviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_enviarActionPerformed(evt);
+            }
+        });
+
+        chb_idade.setText("Maior de Idade");
+
+        grupo_sexo.add(rb_masc);
+        rb_masc.setText("Masculino");
+
+        grupo_sexo.add(rb_fem);
+        rb_fem.setText("Feminino");
+
+        jLabel2.setText("Sexo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(75, 75, 75)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(rb_masc)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(rb_fem))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(btn_enviar)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel1)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(chb_idade, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(txt_nome)))))
+                .addContainerGap(82, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txt_nome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(chb_idade)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rb_masc)
+                    .addComponent(rb_fem)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                .addComponent(btn_enviar)
+                .addGap(28, 28, 28))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_enviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_enviarActionPerformed
+        String idade = "";
+        if (chb_idade.isSelected()) {
+            idade = "Você é maior de idade";
+        } else {
+            idade = "Você é menor de idade";
+        }
+
+        String sexo = "";
+        if (rb_masc.isSelected()) {
+            sexo = "Masculino";
+        } else if (rb_fem.isSelected()) {
+            sexo = "Feminino";
+        } else {
+            sexo = "Sexo não informado";
+        }
+
+        JOptionPane.showMessageDialog(rootPane, "Seu nome é: " + txt_nome.getText() + "\n" + idade+"\n" + "Sexo: " + sexo , "Alerta", JOptionPane.INFORMATION_MESSAGE);
+
+
+    }//GEN-LAST:event_btn_enviarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +163,13 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_enviar;
+    private javax.swing.JCheckBox chb_idade;
+    private javax.swing.ButtonGroup grupo_sexo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JRadioButton rb_fem;
+    private javax.swing.JRadioButton rb_masc;
+    private javax.swing.JTextField txt_nome;
     // End of variables declaration//GEN-END:variables
 }
