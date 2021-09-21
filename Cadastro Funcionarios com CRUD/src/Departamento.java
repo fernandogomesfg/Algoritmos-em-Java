4c87bad82e8b1109ed97c17e122affc09b0fd25c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-21 23:44:05 +0200

    update

--- Cadastro Funcionarios com CRUD/src/Departamento.java
+++ Cadastro Funcionarios com CRUD/src/Departamento.java
@@ -1,13 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Fernando Gomes
  */
 public class Departamento {
+
+    private int Codigo;
+    private String Nome;
+    ArrayList<Funcionario> ListaFunc;
+
+    public Departamento() {
+        ListaFunc = new ArrayList();
+    }
+
+    public Departamento(int Codigo, String Nome) {
+        this.Codigo = Codigo;
+        this.Nome = Nome;
+        ListaFunc = new ArrayList();
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    
+    //Metodo para manipular a lista de funcionarios
+
+    public ArrayList<Funcionario> getListaFunc() {  //Retorna a lista inteira dos funcionarios
+        return ListaFunc;
+    }
+
+    public void setListaFunc(ArrayList<Funcionario> ListaFunc) {
+        this.ListaFunc = ListaFunc;
+    }
+    
+    public void addFunc(Funcionario F){
+        ListaFunc.add(F);
+    }
     
 }
