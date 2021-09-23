3d6bab879846a6532d02a24607af8c3bc86f8d62
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-23 15:44:03 +0200

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* int n = 4;
+        System.out.println( ++n);
+        */
+        
+        int n1=5, n2=2;
+        double res=n1/n2;
+        System.out.println(res);
+    }
+    
+}
