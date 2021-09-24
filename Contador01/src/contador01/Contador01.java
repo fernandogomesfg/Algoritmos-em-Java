af45439bd7fa9c5caf2b457ede8cbbef66428b63
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-09-24 23:56:20 +0200

    Contador01

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contador01;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc <= 10) {
+            cc++;
+            if (cc == 5 || cc == 7) {
+                break;
+            }
+            System.out.println("Cambalhota " + cc);
+
+        }
+    }
+
+}
