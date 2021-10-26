85fdf7270a1b18b49d36f38f433e133facdf56b7
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-26 15:42:18 +0200

    exercicio2

---
+++ #Trabalhos da Faculdade/Ficha 1/Numero2/src/numero2/Numero2.java
@@ -0,0 +1,26 @@
+/**
+ * 2 - Declarar as seguintes variaveis
+ * a) soma, tipo int (inteiro) inicializado com zero
+ * b) beta, tipo long (inteiro longo) inicializado com 1000000
+ * c) raiz, tipo double (real) inicializado com 3.4
+ * d) choose, tipo boolean inicializado com false
+ */
+package numero2;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int soma = 0;
+        long beta = 1000000;
+        double raiz = 3.4;
+        boolean choose = false;
+    }
+    
+}
