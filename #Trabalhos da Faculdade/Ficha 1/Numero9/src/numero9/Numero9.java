b46c2188469519ec19a8ca15163972e620861317
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-28 22:28:06 +0200

    Numero9

---
+++ #Trabalhos da Faculdade/Ficha 1/Numero9/src/numero9/Numero9.java
@@ -0,0 +1,21 @@
+/**
+ * Escreva um programa que calcula a area de uma circunferencia
+ *
+ * Data:28/10/2021
+ */
+package numero9;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //declaracao de variaveis
+    }
+    
+}
