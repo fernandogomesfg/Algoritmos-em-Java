c89a12fd1f4e579423a468e0b2a4d1eae3b32917
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-28 22:21:57 +0200

    Update Numero8.java

--- #Trabalhos da Faculdade/Ficha 1/Numero8/src/numero8/Numero8.java
+++ #Trabalhos da Faculdade/Ficha 1/Numero8/src/numero8/Numero8.java
@@ -1,6 +1,6 @@
 /**
  * Escreva um programa que calcula a area de um rectangulo
- * 
+ *
  * Data:28/10/2021
  */
 package numero8;
@@ -17,17 +17,22 @@ public class Numero8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        //Variaveis de entrada
         double comprimento, largura, area;
-        
+
+        Scanner teclado = new Scanner(System.in);
+
+        //Pedindo ao usuario para inserir o comprimento e a largura
         System.out.print("Digite o comprimento do rectangulo: ");
         comprimento = teclado.nextDouble();
         System.out.print("Digite a largura do rectangulo: ");
         largura = teclado.nextDouble();
-        
+
+        //Calculo da area
         area = comprimento * largura;
-        
-        System.out.println("A area do rectangulo eh "+area);
+
+        //Impressao da area
+        System.out.println("A area do rectangulo eh " + area);
     }
-    
+
 }
