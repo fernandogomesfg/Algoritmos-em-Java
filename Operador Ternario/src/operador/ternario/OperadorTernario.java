cb36a1a31e9ce5b795a221197f06b1a61bbca74c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-28 16:55:58 +0200

    Update OperadorTernario.java

--- Operador Ternario/src/operador/ternario/OperadorTernario.java
+++ Operador Ternario/src/operador/ternario/OperadorTernario.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Algoritmo que exemplifica Operador Ternario
+ * Data:28/10/2021
  */
 package operador.ternario;
 
