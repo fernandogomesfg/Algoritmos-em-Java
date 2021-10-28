1c8956ea5314c063c46ac005effd97b40ca10730
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-28 16:57:18 +0200

    Update ClasseMath.java

--- Classe Math/src/classe/math/ClasseMath.java
+++ Classe Math/src/classe/math/ClasseMath.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Algoritmo com alguns exemplos dos metodos da Classe Math
+ * Data:28/10/2021
  */
 package classe.math;
 
