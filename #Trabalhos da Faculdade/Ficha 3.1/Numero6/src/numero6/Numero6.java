e0f913ff04d6a713ac7ce67381b8b0099332fa2b
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-31 22:50:09 +0200

    Numero6

---
+++ #Trabalhos da Faculdade/Ficha 3.1/Numero6/src/numero6/Numero6.java
@@ -0,0 +1,37 @@
+/**
+ * Qual será a saída do programa abaixo
+ *
+ * public class Ciclo{
+ *  public static void main (String args []){
+ *      boolean a = true;
+ *      int i;
+ *      while(a)
+ *          for(i=0; i<=5; i++)
+ *              System.out.println(i);
+ *      System.out.println("Fim do Ciclo");
+ *  }
+ * }
+ */
+package numero6;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        int i;
+        while (a) {
+            for (i = 0; i <= 5; i++) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("Fim do Ciclo");
+    }
+
+}
