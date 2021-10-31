ebbf4099dce87ce4ebd6f9604e63cb7c6d6eb531
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-31 22:26:06 +0200

    Update Numero1.java

--- #Trabalhos da Faculdade/Ficha 3.1/Numero1/src/numero1/Numero1.java
+++ #Trabalhos da Faculdade/Ficha 3.1/Numero1/src/numero1/Numero1.java
@@ -1,6 +1,6 @@
 /**
  * Faça um programa java para calcular as duas raízes de uma equação do segundo grau, sendo dado os valores dos coeficientes a, b, c
- * 
+ *
  * Data: 31/10/2021
  */
 package numero1;
@@ -18,7 +18,7 @@ public class Numero1 {
      */
     public static void main(String[] args) {
         //Variaveis
-        double a, b, c, x1, x2, delta = 0;
+        double a, b, c, x1, x2, delta;
         Scanner teclado = new Scanner(System.in);
 
         //Inserindo dados com o teclado
@@ -32,24 +32,23 @@ public class Numero1 {
         //Verificando se o valor de a nao eh igual a zero
         if (a != 0) {
             delta = ((b * b) - 4 * a * c);
+            //Exibindo o valor de Delta
+            System.out.println("O valor de Delta eh: " + delta);
+            
+            if (delta < 0) {
+                System.out.println("A equacao nao possui raizes, pois o Delta eh negativo");
+            } else if (delta == 0) {
+                System.out.println("A equacao possui duas raizez distintas");
+                x1 = (-b + (Math.sqrt(delta))) / (2 * a);
+                System.out.println("O valor de x1 eh: " + x1 + " e o x2 eh: " + x1);
+            } else {
+                System.out.println("A equacao possui duas raizes, pois o Delta e maior que zero");
+                x1 = (-b + (Math.sqrt(delta))) / (2 * a);
+                x2 = (-b - (Math.sqrt(delta))) / (2 * a);
+                System.out.println("O valor de x1 eh: " + x1 + " e o x2 eh: " + x2);
+            }
         } else {
             System.out.println("O valor deve ser diferente de zero");
-        }
-        
-        //Exibindo o valor de Delta
-        System.out.println("O valor de Delta eh: " + delta);
-
-        if (delta < 0) {
-            System.out.println("A equacao nao possui raizes, pois o Delta eh negativo");
-        } else if (delta == 0) {
-            System.out.println("A equacao possui duas raizez distintas");
-            x1 = (-b + (Math.sqrt(delta))) / (2 * a);
-            System.out.println("O valor de x1 eh: " + x1 + " e o x2 eh: " + x1);
-        } else {
-            System.out.println("A equacao possui duas raizes, pois o Delta e maior que zero");
-            x1 = (-b + (Math.sqrt(delta))) / (2 * a);
-            x2 = (-b - (Math.sqrt(delta))) / (2 * a);
-            System.out.println("O valor de x1 eh: " + x1 + " e o x2 eh: " + x2);
         }
 
     }
