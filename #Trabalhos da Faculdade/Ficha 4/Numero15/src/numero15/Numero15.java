06956ff7e14e8aece6e16bddd6d46dfd50353fe0
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-07 23:43:20 +0200

    Numero15

---
+++ #Trabalhos da Faculdade/Ficha 4/Numero15/src/numero15/Numero15.java
@@ -0,0 +1,23 @@
+/**
+ * Escreva um programa para testar as seguintes instrucoes
+ *
+ * Data: 07/11/2021
+ */
+package numero15;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Caracteres ASCII e Unicode
+        System.out.println("Letra A(ASCII type):" +'B');
+        System.out.println('\u0041');
+    }
+    
+}
