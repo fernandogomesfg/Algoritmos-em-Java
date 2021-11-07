d1955e0a6f0cbfa69b12e8765d5174812e9dea8f
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-07 23:32:42 +0200

    Numero14

---
+++ #Trabalhos da Faculdade/Ficha 4/Numero14/src/numero14/Numero14.java
@@ -0,0 +1,23 @@
+/**
+ * Usando o formato do programa do exercicio 12, escreva um programa para imprimir as constantes PI e E da Matematica
+ *
+ * Data: 07/11/2021
+ */
+package numero14;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Impressao das constantes PI e E da Matematica");
+        System.out.println("Contante PI: " + Math.PI);
+        System.out.println("Constante E: " + Math.E);
+    }
+
+}
