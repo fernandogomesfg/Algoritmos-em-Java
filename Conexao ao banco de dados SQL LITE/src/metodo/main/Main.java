558e989dc00c5e188410cf40d487757e30b25ec5
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-12 22:28:27 +0200

    Update Main.java

--- Conexao ao banco de dados SQL LITE/src/metodo/main/Main.java
+++ Conexao ao banco de dados SQL LITE/src/metodo/main/Main.java
@@ -5,6 +5,8 @@
  */
 package metodo.main;
 
+import conexao.ModuloConexao;
+
 /**
  *
  * @author Fernando Gomes
@@ -16,6 +18,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ModuloConexao Conexao = new ModuloConexao();
+        Conexao.conectar();
+        Conexao.desconectar();
     }
     
 }
