3fd662ba5ee19c4257c6b829103aa9deb7c96667
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-16 17:25:58 +0200

    Update Numero5.java

--- #Trabalhos da Faculdade/Ficha 3.1/Numero5/src/numero5/Numero5.java
+++ #Trabalhos da Faculdade/Ficha 3.1/Numero5/src/numero5/Numero5.java
@@ -19,19 +19,27 @@ public class Numero5 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        int[] a = {14, 4, 5, 7, 84, 6, 14, 3};
-        int num_maior = a[0];
-        int num_menor = a[0];
-        
+        //int[] a = {14, 4, 5, 7, 84, 6, 14, 3, 4, 0,-14};
+        int[] a = new int[10];
+        //Preencheminto do array com SCANNER
+
+        for (int i = 1; i < a.length; i++) {
+            System.out.print("Digite o " + i + "o numero: ");
+            a[i] = teclado.nextInt();
+
+        }
+
+        //Inicializar uma variavel
+        int num_maior = a[0], num_menor = a[1];
+        //Comparacao dos valores introduzidos no array
         for (int i = 1; i < a.length; i++) {
             if (num_maior < a[i]) {
                 num_maior = a[i];
             }
+
             if (num_menor > a[i]) {
                 num_menor = a[i];
             }
-            
-            
         }
 
         System.out.println("O menor numero menor eh: " + num_menor);
