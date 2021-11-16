0d13b273910a992d680d57990fdcdf76b8096a63
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-16 12:56:01 +0200

    MesDoAno

---
+++ #Trabalhos da Faculdade/Estudo Pessoal/Mes do Ano/src/mes/pkgdo/ano/MesDoAno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mes.pkgdo.ano;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class MesDoAno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
