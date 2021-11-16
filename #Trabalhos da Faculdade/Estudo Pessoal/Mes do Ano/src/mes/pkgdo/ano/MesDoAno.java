5dcb18e96205aed964c3f0bad3faff8946529f54
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-16 13:20:11 +0200

    Update MesDoAno.java

--- #Trabalhos da Faculdade/Estudo Pessoal/Mes do Ano/src/mes/pkgdo/ano/MesDoAno.java
+++ #Trabalhos da Faculdade/Estudo Pessoal/Mes do Ano/src/mes/pkgdo/ano/MesDoAno.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Ler um número inteiro entre 1 e 12 e escrever o mês correspondente. Caso o número seja fora desse intervalo, informar que não existe mês com este número
  */
 package mes.pkgdo.ano;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +15,53 @@ public class MesDoAno {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int op;
+
+        System.out.println("Introduza um numero de 1-12");
+        op = teclado.nextInt();
+
+        switch (op) {
+            case 1:
+                System.out.println("Jaeiro");
+                break;
+            case 2:
+                System.out.println("Fevereiro");
+                break;
+            case 3:
+                System.out.println("Marco");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Maio");
+                break;
+            case 6:
+                System.out.println("Junho");
+                break;
+            case 7: 
+                System.out.println("Julho");
+                break;
+            case 8: 
+                System.out.println("Agosto");
+                break;
+            case 9: 
+                System.out.println("Setembro");
+                break;
+            case 10:
+                  System.out.println("Outubro");
+                  break;
+            case 11:
+                System.out.println("Novemro");
+                break;
+            case 12:
+                System.out.println("Dezembro");
+            default:
+                System.out.println("ERRO");
+                        
+            
+        }
     }
-    
+
 }
