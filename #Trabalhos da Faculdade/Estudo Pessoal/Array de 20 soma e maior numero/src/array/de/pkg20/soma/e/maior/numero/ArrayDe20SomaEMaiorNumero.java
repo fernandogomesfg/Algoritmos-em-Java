7e6bf64d698fa59fc60d1bd8651ca382801bf11f
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-18 12:53:33 +0200

    ArrayDe20SomaEMaiorNumero

---
+++ #Trabalhos da Faculdade/Estudo Pessoal/Array de 20 soma e maior numero/src/array/de/pkg20/soma/e/maior/numero/ArrayDe20SomaEMaiorNumero.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array.de.pkg20.soma.e.maior.numero;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class ArrayDe20SomaEMaiorNumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
