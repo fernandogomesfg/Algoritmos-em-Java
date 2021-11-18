1e70b7678d499e5fd070a091c607b9a606398c78
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-18 13:54:39 +0200

    Update SomaNoArray.java

--- #Trabalhos da Faculdade/Estudo Pessoal/Soma no Array/src/soma/no/array/SomaNoArray.java
+++ #Trabalhos da Faculdade/Estudo Pessoal/Soma no Array/src/soma/no/array/SomaNoArray.java
@@ -18,17 +18,50 @@ public class SomaNoArray {
      */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
-        int[] a = new int[10];
-        
+
+        int[] a = new int[3];
+
         //Preencheminto do array com SCANNER
-        for (int i = 1; i < a.length; i++) {
-            System.out.print("Digite o " + i + "o numero: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Digite o " + (i + 1) + "o numero: ");
             a[i] = teclado.nextInt();
 
         }
-        
+
         //impressao do aray
+        System.out.println("");
+        System.out.println("Os elemntos do array sao:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        //maior elemento do array
+        int maior = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (maior < a[i]) {
+                maior = a[i];
+
+            }
+        }
+        System.out.println("\n");
+        System.out.println("O maior valor do array eh: " + maior);
+
+        //soma dos elementos do array
+        int soma = 0;
+        for (int i = 0; i < a.length; i++) {
+            soma = soma + a[i];
+        }
+        System.out.println("");
+        System.out.println("A soma dos valores do array eh: " + soma);
+
+        //multiplicacao dos elementos do array
+        int multiplicacao = 1;
+        for (int i = 0; i < a.length; i++) {
+            multiplicacao = multiplicacao * a[i];
+
+        }
+        System.out.println("");
+        System.out.println("A multiplicacao dos valores do array eh: " + multiplicacao);
     }
-    
+
 }
