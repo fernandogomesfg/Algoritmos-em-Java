5ee8d0605683c2b7e0c63ba82e816457059f6c87
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-18 23:36:25 +0200

    calculadora POO

---
+++ #Trabalhos da Faculdade/Estudo Pessoal/Calculadora POO/src/Calculadora.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Calculadora {
+
+    //atributos
+    public double num1, num2;
+    public char op;
+
+    //construtor
+    public Calculadora(double num1, double num2, char op) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.op = op;
+    }
+
+    public void calcular() {
+        if (this.op == '+') {
+            System.out.println(this.num1 + this.num2);
+        }else if (this.op == '-') {
+            System.out.println(this.num1 - this.num2);
+        }else if (this.op == '*') {
+            System.out.println(this.num1 * this.num2);
+        }else if (this.op == '/') {
+            if (this.num2 == 0) {
+                System.out.println("Impossivel dividir por zero");
+            }else{
+                System.out.println(this.num1 / this.num2);
+            }
+            
+        }
+        
+    }
+
+}
---
+++ #Trabalhos da Faculdade/Estudo Pessoal/Calculadora POO/src/teste.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class teste extends Calculadora{
+    public static void main(String[] args) {
+        Calculadora c1 = new Calculadora(10, 0, '/');
+        c1.calcular();
+        
+    }
+
+    public teste(double num1, double num2, char op) {
+        super(num1, num2, op);
+    }
+    
+}
