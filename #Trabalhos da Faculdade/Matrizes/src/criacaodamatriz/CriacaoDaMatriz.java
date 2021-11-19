04b8b81d0c00f6081c199516859330467beca872
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-11-19 21:43:31 +0200

    Update CriacaoDaMatriz.java

--- #Trabalhos da Faculdade/Matrizes/src/criacaodamatriz/CriacaoDaMatriz.java
+++ #Trabalhos da Faculdade/Matrizes/src/criacaodamatriz/CriacaoDaMatriz.java
@@ -3,24 +3,21 @@ package criacaodamatriz;
 import java.util.Scanner;
 
 public class CriacaoDaMatriz {
-    public static void main(String[]args){
-       
-        Scanner teclado= new Scanner (System.in);
-        int[][] matriz= new int[2][2];
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int[][] matriz = new int[2][2];
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
                 matriz[i][j] = teclado.nextInt();
-                
             }
-            
         }
-        
+
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                System.out.println(matriz[i][j]+"\t");
-                
+                System.out.println(matriz[i][j] + "\t");
             }
-            
         }
     }
 }
