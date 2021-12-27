387eaf069dd50887edf5f34e60e5e29b373150e8
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2021-12-27 19:38:57 +0200

    numero1

---
+++ #Trabalhos da Faculdade/Exame Normal 2016/Numero1/src/numero1/Numero1.java
@@ -0,0 +1,26 @@
+
+package numero1;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Numero1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x = 0, y = 0, z = 0;
+       
+       //A
+       //Math.abs(x-y/z);
+       
+       //b
+       //Math.sqrt(x-1+x*y);
+       
+       //c
+       System.out.println(Math.exp(1));
+    }
+    
+}
