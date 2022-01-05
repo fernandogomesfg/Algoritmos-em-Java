1d59a68f75a4dd471efa845652e5c0145828e129
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-01-05 14:08:27 +0200

    Update Director.java

--- #Trabalhos da Faculdade/Exame Normal 2016/Director/src/Director.java
+++ #Trabalhos da Faculdade/Exame Normal 2016/Director/src/Director.java
@@ -1,13 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Fernando Gomes
  */
 public class Director {
-    
+
+    //atriutos
+    public String nome, departamento;
+    public double salario;
+
+    //construtor
+    public Director(String nome, String departamento, double salario) {
+        this.nome = nome;
+        this.departamento = departamento;
+        this.salario = salario;
+    }
+
+    public Director() {
+
+    }
+
+    //metodos de acesso aos atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    //metodo altera salario
+    public void alteraSalario(double novoSalario) {
+        salario = novoSalario;
+        System.out.println("O salario do" + getNome() + " foi alterado para " + salario);
+    }
+
+    //print
+    public void print() {
+        System.out.println("Dados do director");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Departamento: " + getDepartamento());
+        System.out.println("Salario: " + getSalario());
+    }
+
 }
