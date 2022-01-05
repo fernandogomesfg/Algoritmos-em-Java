074f761ee49fcd88a04c37a207ceb7c9eca9f6e1
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-01-05 14:08:42 +0200

    Create teste.java

---
+++ #Trabalhos da Faculdade/Exame Normal 2016/Director/src/teste.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class teste {
+
+    public static void main(String[] args) {
+        Director d1 = new Director("Fernando Gomes", "DMI", 50000);
+        Director d2 = new Director();
+        d1.print();
+        System.out.println("\n");
+        d1.alteraSalario(500);
+        System.out.println("\n");
+        d1.print();
+        
+        Director[] directores = new Director[2];
+        //preenchimento
+        for (int i = 0; i < directores.length; i++) {
+            directores[i] = new Director(JOptionPane.showInputDialog("Nome"), JOptionPane.showInputDialog("Departamento"), Double.parseDouble(JOptionPane.showInputDialog("Salario")));
+            
+        }
+    }
+}
