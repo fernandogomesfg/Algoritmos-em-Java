36f03e89e77dca35dbf65b0a73d8764b4ee4598b
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-03-25 11:06:31 -0700

    Update RecursosHumanos.java

--- #Programacao Orientada a Objectos/Colaborador/src/RecursosHumanos.java
+++ #Programacao Orientada a Objectos/Colaborador/src/RecursosHumanos.java
@@ -5,42 +5,57 @@
  */
 public class RecursosHumanos {
 
-    public static void main(String[] args) {
+    public static Colaborador[] colaboradores = new Colaborador[3];
+
+    public static void addAdministrador() {
+        colaboradores[0] = new Administrador("Fernando Gomes Administrador", "Matola", "870357939", 3, 15000, 0.1, 2000);
+    }
+
+    public static void listaAdministrador() {
+        //impressao dos administradores
+        System.out.println("\nAdministradores");
+        for (int i = 0; i < colaboradores.length; i++) {
+            if (colaboradores[i] instanceof Administrador) {
+                System.out.println(colaboradores[i].toString()
+                );
+            }
+
+        }
+
+    }
+
+    public static void addOperario() {
+        colaboradores[1] = new Operario("Fernando Gomes Operario", "Gaza", "8412345678", 4, 3000, 0.1, 1000, 0.05);
+    }
+
+    public static void listaOperario() {
+        System.out.println("\nOperarios");
+        for (int i = 0; i < colaboradores.length; i++) {
+            if (colaboradores[i] instanceof Operario) {
+                System.out.println(colaboradores[i].toString()
+                );
+            }
+
+        }
+    }
+
+    public static void addColaborador() {
+        colaboradores[3] = new Colaborador("Fernando Gomes Colaborador", "Maputo", "840357939", 1, 5000, 0.1);
 
         //criando um colaborador
         System.out.println("Colaborador:");
-        Colaborador fernando = new Colaborador("Fernando Gomes Colaborador", "Maputo", "840357939", 1, 5000, 0.1);
-        System.out.println(fernando.toString());
-        System.out.println("O salario: " + fernando.calcularSalario() + "\n");
 
-        //criando um administrador
-        System.out.println("Administrador:");
-        Colaborador admin = new Administrador("Fernando Gomes Administrador", "Matola", "870357939", 3, 15000, 0.1, 2000);
-        System.out.println(admin.toString());
-        System.out.println("O salario do Administrador: " + admin.calcularSalario() + "\n");
+        System.out.println(colaboradores[3].toString());
+        System.out.println("O salario: " + colaboradores[3].calcularSalario() + "\n");
 
-        //criando um operario
-        System.out.println("Operario:");
-        Colaborador operario = new Operario("Fernando Gomes Operario", "Gaza", "8412345678", 4, 3000, 0.1, 1000, 0.05);
-        System.out.println(operario.toString());
-        System.out.println("O salario do operario: " + operario.calcularSalario());
-        System.out.println("");
+    }
 
-        //criacao de um array de objectos da classe colaborador
-        Colaborador[] colaboradores = new Colaborador[3];   
-        colaboradores[0] = fernando;
-        colaboradores[1] = admin;
-        colaboradores[2] = operario;
+    public static void main(String[] args) {
+        addAdministrador();
+        listaAdministrador();
 
-        //impressao de todos colaboradores
-        System.out.println("Impressao usando array");
-        for (int i = 0; i < colaboradores.length; i++) {
-            System.out.println(colaboradores[i].toString());
-        }
-        
-
-
-
+        addOperario();
+        listaOperario();
 
     }
 }
