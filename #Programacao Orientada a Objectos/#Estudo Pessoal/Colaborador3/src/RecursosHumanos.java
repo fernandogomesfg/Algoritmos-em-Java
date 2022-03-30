72d52abbd88020dfb6c698d433967c5b5f665207
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-03-30 08:12:38 -0700

    Create RecursosHumanos.java

---
+++ #Programacao Orientada a Objectos/#Estudo Pessoal/Colaborador3/src/RecursosHumanos.java
@@ -0,0 +1,30 @@
+
+import javax.swing.JOptionPane;
+
+
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class RecursosHumanos {
+    
+    public static Colaborador[] colaboradores = new Colaborador[3];
+    
+    
+    //metodo para adicionar administrador
+    public static void addAdministrador(){
+        Colaborador[0] = new Administrador(JOptionPane.showInputDialog("Nome do Administrador"), JOptionPane.showInputDialog("Endereco do administrador"), JOptionPane, 0, 0, 0, 0)
+    }
+    
+    
+    
+    
+    
+    public static void main (String[] args){
+       
+    }
+            
+            
+    
+}
