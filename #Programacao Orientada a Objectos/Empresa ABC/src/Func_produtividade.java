e7f66c01b75d9e0825ec3c4553268954c8678440
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-03-31 17:06:09 -0700

    Create Func_produtividade.java

---
+++ #Programacao Orientada a Objectos/Empresa ABC/src/Func_produtividade.java
@@ -0,0 +1,50 @@
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Func_produtividade extends Funcionario {
+
+    //atributis
+    private int unidade_produzida;
+    private double valor_unidade_produzida;
+
+    //construtor
+    public Func_produtividade(int unidade_produzida, double valor_unidade_produzida, String nome, String num_BI, String estado_civil, String data_nasc, String morada, String nome_pai, String nome_mae, String data_ingresso, char sexo, double altura, double salario) {
+        super(nome, num_BI, estado_civil, data_nasc, morada, nome_pai, nome_mae, data_ingresso, sexo, altura, salario);
+        this.unidade_produzida = unidade_produzida;
+        this.valor_unidade_produzida = valor_unidade_produzida;
+    }
+
+    //GET e SET
+    public int getUnidade_produzida() {
+        return unidade_produzida;
+    }
+
+    public void setUnidade_produzida(int unidade_produzida) {
+        this.unidade_produzida = unidade_produzida;
+    }
+
+    public double getValor_unidade_produzida() {
+        return valor_unidade_produzida;
+    }
+
+    public void setValor_unidade_produzida(double valor_unidade_produzida) {
+        this.valor_unidade_produzida = valor_unidade_produzida;
+    }
+
+    //metodo toString
+    @Override
+    public String toString() {
+        return "Func_produtividade{" + "unidade_produzida=" + unidade_produzida + ", valor_unidade_produzida=" + valor_unidade_produzida + '}';
+    }
+
+    //metodo para calcular salario do produvidade
+    @Override
+    public double calcularRemuneracao() {
+        return this.getSalario() + (this.getValor_unidade_produzida() * 0.1);   //duvida
+    }
+    
+    
+
+}
