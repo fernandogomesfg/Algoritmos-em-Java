819e53edd42e2cd325d3993a0ac7553d6937c0e0
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-03-31 17:05:57 -0700

    Create FolhaSalario.java

---
+++ #Programacao Orientada a Objectos/Empresa ABC/src/FolhaSalario.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class FolhaSalario {
+   
+    //array de funcionarios
+    public static Funcionario[] funcionarios = new Funcionario[3];
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int op = 0;
+        
+                    
+        
+    
+    } 
+}
