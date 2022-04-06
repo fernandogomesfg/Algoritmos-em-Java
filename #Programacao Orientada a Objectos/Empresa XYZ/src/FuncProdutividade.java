5928f59199885afed9fadf3eb65518b560389812
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-06 12:43:42 -0700

    Update FuncProdutividade.java

--- #Programacao Orientada a Objectos/Empresa XYZ/src/FuncProdutividade.java
+++ #Programacao Orientada a Objectos/Empresa XYZ/src/FuncProdutividade.java
@@ -35,7 +35,7 @@ public class FuncProdutividade extends Funcionario{
     //toString
     @Override
     public String toString() {
-        return "FuncProdutividade{" + "unidade_produzida=" + unidade_produzida + ", valor_unidade_produzida=" + valor_unidade_produzida + '}';
+        return super.toString() + "Unidade produzidas=" + unidade_produzida + ", Valor por Unidade_produzida=" + valor_unidade_produzida + '}';
     }
     
     //metodo para calcular salario do produtividade
