6692c596e3792819cca8b8b365cce08a05d7a52c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-06 12:32:24 -0700

    Create Funcionario.java

---
+++ #Programacao Orientada a Objectos/Funcionario/src/Funcionario.java
@@ -0,0 +1,56 @@
+
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public abstract class Funcionario {
+    //atributos
+    private String num_BI, data_ingresso;
+    private double salario_fixo;
+    
+    //construtor
+    public Funcionario(String num_BI, String data_ingresso, double salario_fixo) {
+        this.num_BI = num_BI;
+        this.data_ingresso = data_ingresso;
+        this.salario_fixo = salario_fixo;
+    }
+    
+    //GET e SET
+    public String getNum_BI() {
+        return num_BI;
+    }
+
+    public void setNum_BI(String num_BI) {
+        this.num_BI = num_BI;
+    }
+
+    public String getData_ingresso() {
+        return data_ingresso;
+    }
+
+    public void setData_ingresso(String data_ingresso) {
+        this.data_ingresso = data_ingresso;
+    }
+
+    public double getSalario_fixo() {
+        return salario_fixo;
+    }
+
+    public void setSalario_fixo(double salario_fixo) {
+        this.salario_fixo = salario_fixo;
+    }
+    
+    //metodo toString
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "num_BI=" + num_BI + ", data_ingresso=" + data_ingresso + ", salario_fixo=" + salario_fixo + '}';
+    }
+    
+    
+    //metodo abstrato (so possui cabecalho) 
+    public abstract double calcularRemuneracao();
+    
+    
+}
