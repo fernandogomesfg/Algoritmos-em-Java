e788e6e54310a45211340034f3e7e42ddd49efed
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-04-28 21:12:12 -0700

    Create Bicicleta.java

---
+++ #Trabalhos da Faculdade/Estudo Pessoal/Veiculo/src/Bicicleta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class Bicicleta  extends Veiculo{
+
+    //Atributos
+    //
+    public Bicicleta(String marca, String modelo, String cor, double preço) {
+        super(marca, modelo, cor, preço);
+    }
+
+    //get e set
+    
+    //toStrinng
+    @Override
+    public String toString() {
+        return "Bicicleta{" + toString();
+    }
+    
+
+}
