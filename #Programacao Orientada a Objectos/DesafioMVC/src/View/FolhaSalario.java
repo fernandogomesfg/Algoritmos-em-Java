e3a06b5b52d378edf27473611ff623bd2cf4d564
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-05-03 10:35:20 -0700

    Create FolhaSalario.java

---
+++ #Programacao Orientada a Objectos/DesafioMVC/src/View/FolhaSalario.java
@@ -0,0 +1,19 @@
+
+package View;
+
+import static Controller.Controller.*;
+//import static Controller.Controller.addFuncComissionado;
+//import static Controller.Controller.listFuncComissionario;
+
+/**
+ *
+ * @author Fernando Gomes
+ */
+public class FolhaSalario {
+    public static void main(String[] args) {
+        addFuncComissionado();
+        listFuncComissionario();
+        
+    }
+    
+}
