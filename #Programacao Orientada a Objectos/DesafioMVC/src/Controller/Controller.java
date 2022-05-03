2cd141a18f8697a8cf550ba58d9a24ab29c58e97
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-05-03 11:11:05 -0700

    Update Controller.java

--- #Programacao Orientada a Objectos/DesafioMVC/src/Controller/Controller.java
+++ #Programacao Orientada a Objectos/DesafioMVC/src/Controller/Controller.java
@@ -41,7 +41,8 @@ public class Controller {
             objGravar.close();
             folhasalario.flush();
             folhasalario.close();
-            System.out.println("Objeto gravado com sucesso!");
+            //System.out.println("Objeto gravado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Funcionario Comissionado gravado com sucesso", "Funcionario Comissionado", JOptionPane.INFORMATION_MESSAGE);
             
             
         } catch (Exception ex) {
@@ -60,7 +61,8 @@ public class Controller {
             objGravar.close();
             folhasalario.flush();
             folhasalario.close();
-            System.out.println("Objeto gravado com sucesso!");
+            //System.out.println("Objeto gravado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Funcionario Comissionado gravado com sucesso", "Funcionario Comissionado", JOptionPane.INFORMATION_MESSAGE);
             } catch (Exception e) {
                 
             }
@@ -80,6 +82,83 @@ public class Controller {
             System.out.println("Funcionarios Comissionarios");
             for (int i = 0; i < funcionarios.size(); i++) {
                 if (funcionarios.get(i) instanceof FuncComissionado) {
+                    System.out.println(funcionarios.get(i).toString());
+                }
+
+            }
+
+            objLeitura.close();
+            folhasalario.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /////////////////////////////////////////////////
+    //metodo para adicionar funcionario comissionado
+    public static void addFuncProdutividade() {
+        
+        try {
+            
+            //Carrega o arquivo
+            FileInputStream folha_salario = new FileInputStream("folhasalario.dat");
+            //Classe responsavel por recuperar os objetos do arquivo
+            ObjectInputStream objLeitura
+                    = new ObjectInputStream(folha_salario);
+            funcionarios = (ArrayList) objLeitura.readObject();
+            funcionarios.add(new FuncProdutividade(JOptionPane.showInputDialog("Numero do BI do funcionario produtividade"), JOptionPane.showInputDialog("Data de ingresso do funcionario produtividade"), Double.parseDouble(JOptionPane.showInputDialog("Salario do funcionario produtividade")), Integer.parseInt(JOptionPane.showInputDialog("Unidade produzida do funcionario produtividade")), Double.parseDouble(JOptionPane.showInputDialog("Valor da unidade produzida funcionario produtividade"))));
+            
+                //Gera o arquivo para armazenar o objeto
+            FileOutputStream folhasalario
+                    = new FileOutputStream("folhasalario.dat");
+            //Classe responsavel por inserir os objetos
+            ObjectOutputStream objGravar = new ObjectOutputStream(folhasalario);
+            //Grava o objeto cliente no arquivo
+            objGravar.writeObject(funcionarios);
+            objGravar.flush();
+            objGravar.close();
+            folhasalario.flush();
+            folhasalario.close();
+            //System.out.println("Objeto gravado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Funcionario Produtividade gravado com sucesso", "Funcionario Produtividade", JOptionPane.INFORMATION_MESSAGE);
+            
+            
+        } catch (Exception ex) {
+            try {
+                //Gera o arquivo para armazenar o objeto se nao existir
+            FileOutputStream folhasalario
+                    = new FileOutputStream("folhasalario.dat");
+            //Classe responsavel por inserir os objetos
+            ObjectOutputStream objGravar = new ObjectOutputStream(folhasalario);
+            
+            funcionarios.add(new FuncProdutividade(JOptionPane.showInputDialog("Numero do BI do funcionario produtividade"), JOptionPane.showInputDialog("Data de ingresso do funcionario produtividade"), Double.parseDouble(JOptionPane.showInputDialog("Salario do funcionario produtividade")), Integer.parseInt(JOptionPane.showInputDialog("Unidade produzida do funcionario produtividade")), Double.parseDouble(JOptionPane.showInputDialog("Valor da unidade produzida funcionario produtividade"))));
+            
+            //Grava o objeto cliente no arquivo
+            objGravar.writeObject(funcionarios);
+            objGravar.flush();
+            objGravar.close();
+            folhasalario.flush();
+            folhasalario.close();
+            System.out.println("Objeto gravado com sucesso!");
+            } catch (Exception e) {
+                
+            }
+        }
+    }
+    
+    //metodo para listar funcionarios comissionarios
+    public static void listFuncProdutividade() {
+        try {
+            //Carrega o arquivo
+            FileInputStream folhasalario = new FileInputStream("folhasalario.dat");
+            //Classe responsavel por recuperar os objetos do arquivo
+            ObjectInputStream objLeitura
+                    = new ObjectInputStream(folhasalario);
+            funcionarios = (ArrayList) objLeitura.readObject();
+
+            System.out.println("Funcionarios Comissionarios");
+            for (int i = 0; i < funcionarios.size(); i++) {
+                if (funcionarios.get(i) instanceof FuncProdutividade) {
                     System.out.println(funcionarios.get(i).toString());
                 }
 
