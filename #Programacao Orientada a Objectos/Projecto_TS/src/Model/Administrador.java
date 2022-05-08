79d2f61d45ceeea5b1d04cd2548e72385c762986
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-05-07 19:19:43 -0700

    Update Administrador.java

--- #Programacao Orientada a Objectos/Projecto_TS/src/Model/Administrador.java
+++ #Programacao Orientada a Objectos/Projecto_TS/src/Model/Administrador.java
@@ -1,20 +1,20 @@
-
 package Model;
 
 /**
  *
  * @author Antonio Macarringue, Fernando Gomes, Helio Langa & Samuel Ouana
  */
-public class Administrador extends Usuario{
+public class Administrador extends Usuario {
+
     //atributos
     private String sector;
-    
+
     //construtor
     public Administrador(String sector, String nome, String endereco, String num_telefone, int idade) {
         super(nome, endereco, num_telefone, idade);
         this.sector = sector;
     }
-    
+
     //GET e SET
     public String getSector() {
         return sector;
@@ -23,14 +23,11 @@ public class Administrador extends Usuario{
     public void setSector(String sector) {
         this.sector = sector;
     }
-    
-    
+
     //toStrig
     @Override
     public String toString() {
-        return  super.toString() + "Administrador{" + "sector=" + sector + '}';
+        return super.toString() + "Administrador{" + "sector=" + sector + '}';
     }
-    
-    
-    
+
 }
